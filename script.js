cdd2d3829f5fe21979b649802d843daa14d22463
timihendrix33 <t.stevenson33@gmail.com>
2014-11-26 19:23:01 -0500

    working on pulling in json content

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-	jQuery.ajax({
-		url: "stats.json",
-		dataType: 'json',
-		sucess: function(){
-			$('')
-		}
+	$("nav").on("click", "li", function() {
+		var dataRel = $(this).attr('data-rel');
+		console.log(dataRel);
+		$.getJSON('stats.json', function(json){
+			var data = json[0];
+			$('.container #visits .data').html('<h1>' + data[0].count + '</h1>');
+			$('.container #visits .data').append('<h3>'+ data[0].countTxt +'</h3>');
+			$('.container #visits .data').append('<p>'+ data[0].participantsTxt +'</p>');
+			$('.container #visits .data').append('<h2>'+ data[0].participantCount +'</h2>');
+		});
 	});
+
 });
+
