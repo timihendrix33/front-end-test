f20f01da06b80f7483e6df5b7d75d46c9900e0e2
Timothy.Stevenson <t.stevenson33@gmail.com>
2014-12-22 11:53:08 -0500

    working on transition

--- script.js
+++ script.js
@@ -19,35 +19,54 @@ $(document).ready(function(){
 				output += 		"<div class='icon'></div>";
 				output += 	"</div>";
 				output += 	"<div class='data'>";
+				if(data[i].count.indexOf("min") == -1) {
 				output += 		 "<h1 class='count'>" + data[i].count +"</h1>";
+				} else {
+				output += 		 "<h1 class='count'>" + data[i].count +"</h1><span>min</span>";
+				}	
 				output += 		"<h3>" + data[i].countTxt + "</h3>";
 				output += 		"<p>"+ data[i].participantsTxt +"</p>";
 				output += 		"<h2 class='count'>"+ data[i].participantCount +"</h2>";
 				output += 	"</div>";
 				output += 	"</div>";
 				output += "</section>";
-			}
 
+				var dataCount = data[i].count;
+				console.log(dataCount);
+				var removeMin = function(datacount) {
+					newDataCount = datacount.replace(/(min)/g, 'anything');
+					
+					// console.log(datacount);					
+				}
+				removeMin(dataCount);
+	
+			}
+			$(".content section").hide();
 			$(".content")
-				.hide()
 				.html(output)
-				.fadeIn(300);
-			$(".content:contains('mins')").html(function(_, html){
-				return html.replace(/(mins)/g, '<span>mins</span>');
-			});				
-			$(".content:contains('min')").html(function(_, html){
-				return html.replace(/(min)/g, '<span>min</span>');
-			});
-			$('.count').each(function () {
-			  var $this = $(this);
-			  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-			    duration: 500,
-			    easing: 'swing',
-			    step: function () {
-			      $this.text(Math.ceil(this.Counter));
-			    }
-			  });
-			});		
+				.fadeIn(1000);
+			// $(".content:contains('mins')").html(function(_, html){
+			// 	return html.replace(/(mins)/g, '<span>mins</span>');
+			// });				
+			// $(".content:contains('min')").html(function(_, html){
+			// 	return html.replace(/(min)/g, '<span>min</span>');
+			// });
+			// if(".count")
+			// $(".count").counterUp();
+
+
+
+
+			// $('.count').each(function () {
+			//   var $this = $(this);
+			//   jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+			//     duration: 500,
+			//     easing: 'swing',
+			//     step: function () {
+			//       $this.text(Math.ceil(this.Counter));
+			//     }
+			//   });
+			// });		
 
 		});
 	});
